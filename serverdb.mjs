53ea970d9090359ae58de5ea5a8b60c88ed725c2
gmihaic <godri.mihai+github@gmail.com>
2022-04-25 13:57:17 +0300

    check config postgres DATABASE_URL

--- serverdb.mjs
+++ serverdb.mjs
@@ -9,6 +9,30 @@ export default class databaseHandler {
     }
 
     connect() {
+
+        //postgres://aifbwyhawkbluv:89af05513792017733e4eed809234260bb434c2bc670f2d59df4617a202d2d28@ec2-3-209-124-113.compute-1.amazonaws.com:5432/dejfdjtb11k4v3
+        if (process?.env?.DATABASE_URL) {
+
+            if (process.env.DATABASE_URL.substr(0, 11) !== "postgres://") {
+                throw "Invalid DATABASE_URL";
+            }
+
+            const sqlConnectionData = process.env.DATABASE_URL.substr(11).split(":");
+
+            if (sqlConnectionData.length != 3) {
+                throw "Invalid DATABASE_URL";
+            }
+
+            const sqlSplitPassHost = sqlConnectionData[1].split("@");
+            const sqlSplitPorName = sqlConnectionData[2].split("/");
+
+            process.env.SQLDatabaseHost = sqlSplitPassHost[1];
+            process.env.SQLDatabasePort = sqlSplitPorName[0];
+            process.env.SQLDatabaseUser = sqlConnectionData[0];
+            process.env.SQLDatabasePass = sqlSplitPassHost[0];
+            process.env.SQLDatabaseName = sqlSplitPorName[1];            
+        }      
+
         if (!process?.env?.databaseMode || (process.env.databaseMode !== "PostgreSQL" && process.env.databaseMode !== "MongoDB")) {
             throw new Error("Could not init database connection");
         }
